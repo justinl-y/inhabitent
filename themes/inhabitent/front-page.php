9cf1e3f1c208f9b1fddb301237a071e316a0f397
justinl-y <j@solaris.org.uk>
2016-11-06 22:00:45 -0800

    Front page hero banner and basic styling added

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -7,8 +7,14 @@
 
 get_header('main'); ?>
 
+    <section class="home-banner">
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/logos/inhabitent-logo-full.svg" alt="Image of Inhabitent logo" />
+        </a>
+    </section>
+
     <div class="container">
-        <h1>front-page.php</h1>
+
     </div>
 
 <?php get_footer('main'); ?>
