2885c3a6f37143a98833bfed14f3e6a440facb57
justinl-y <j@solaris.org.uk>
2016-11-09 20:16:06 -0800

    Archive title shortened to just title.

--- themes/inhabitent/inc/extras.php
+++ themes/inhabitent/inc/extras.php
@@ -109,3 +109,22 @@ function get_all_product_posts ($query) {
 	}
 }
 add_action( 'pre_get_posts', 'get_all_product_posts' );
+
+
+/*** function to remove “Category:”, “Tag:”, “Author:”, “Archives:” and “Other taxonomy name:” in the archive title ***/
+function my_theme_archive_title( $title ) {
+	if ( is_category() ) {
+		$title = single_cat_title( '', false );
+	} elseif ( is_tag() ) {
+		$title = single_tag_title( '', false );
+	} elseif ( is_author() ) {
+		$title = '<span class="vcard">' . get_the_author() . '</span>';
+	} elseif ( is_post_type_archive() ) {
+		$title = post_type_archive_title( '', false );
+	} elseif ( is_tax() ) {
+		$title = single_term_title( '', false );
+	}
+
+	return $title;
+}
+add_filter( 'get_the_archive_title', 'my_theme_archive_title' );
