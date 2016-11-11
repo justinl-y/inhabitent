c72fe09eda34fc8b29b39d72dc07fb77f975475a
justinl-y <j@solaris.org.uk>
2016-11-10 19:41:31 -0800

    Adventure post type pages added

---
+++ themes/inhabitent/archive-adventure.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ *
+ * Template Name: Adventures Page
+ *
+**/
+
+get_header('main'); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<div class="container">
+
+				<p>archive-adventure.php</p>
+
+				<?php if ( have_posts() ) : ?>
+
+					<header class="page-header">
+						<?php
+							the_archive_title( '<h1 class="page-title">', '</h1>' );
+							the_archive_description( '<div class="taxonomy-description">', '</div>' );
+						?>
+					</header><!-- .page-header -->
+
+
+					<?php //while ( have_posts() ) : the_post(); ?>
+						<?php //get_template_part( 'template-parts/content' ); ?>
+					<?php //endwhile; ?>
+
+
+					<div class="archive-product-grid">
+						<?php while ( have_posts() ) : the_post(); ?>
+							<div class="single-product-block">
+
+								<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+									<header class="entry-header">
+
+										<!--images and url-->
+										<div class="thumbnail-wrapper">
+											<a href="<?php echo get_permalink(); ?> ">
+												<?php if ( has_post_thumbnail() ) : ?>
+													<?php the_post_thumbnail( 'large' ); ?>
+												<?php endif; ?>
+											</a>
+										</div>
+
+										<!--title and price-->
+										<div class="product-info">
+											<div class="product-title"><?php the_title(); ?></div>
+											<div class="product-price"><?php echo CFS()->get( 'product_price' ); ?></div>
+										</div>
+
+									</header><!-- .entry-header -->
+
+								</article><!-- #post-## -->
+
+							</div><!-- .single-product-block -->
+						<?php endwhile; ?>
+					</div><!-- .archive-product-grid -->
+
+
+
+					<?php the_posts_navigation(); ?>
+
+				<?php else : ?>
+
+					<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+				<?php endif; ?>
+
+				<aside>
+					<?php get_sidebar(); ?>
+				</aside>
+
+			</div><!-- .container -->
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer('main'); ?>
+
+
---
+++ themes/inhabitent/single-adventure.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header('main'); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<button type="button" id="close-comments">Close Comments</button>
+
+			<div class="container">
+				<p>single-adventure.php</p>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'template-parts/content', 'single' ); ?>
+
+					<?php the_post_navigation(); ?>
+
+					<?php
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
+					?>
+
+				<?php endwhile; // End of the loop. ?>
+			</div><!-- .container -->
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+	<aside>
+		<?php get_sidebar(); ?>
+	</aside>
+
+<?php get_footer('main'); ?>
