6dac9ed61bac6193686c9b3ae82d2a093ae00e34
justinl-y <j@solaris.org.uk>
2016-11-11 22:18:24 -0800

    Header search toggle/slide JS functionality modified

--- themes/inhabitent/js/inhabitent.js
+++ themes/inhabitent/js/inhabitent.js
@@ -1,37 +1,9 @@
 (function( $ ) {
 
-    //$( "#search-toggle" ).click(function() {
-        //$( "#search-field" ).slideToggle( "slow", function() {
-            // Animation complete.
-        //});
-    //});
-
-
-    //$("#search-toggle").click(function () {
-        //$("#search-field").hide();
-    //});
-
-
     $('#search-toggle').on('click', function() {
-        $('#search-field').animate( { width: 'toggle' }, 500 );
-
-        //$('#main-menu-navigation-container').animate({right: '80px'});
+        $('#search-field').animate( { width: 'toggle' }, 500 ).focus();
     });
 
-    $('#search-field').animate( { width: 'toggle' }, 500 );
+    $('#search-field').animate( { width: 'toggle' }, 0 );
 
 })( jQuery );
-
-
-
-/*var sideMenu = false;
-$("#wel1").click(function() {
-    if (!sideMenu) {
-        $("#sidemenu").animate({left: "80px"});
-        sideMenu = true;
-    }
-    else {
-        $("#sidemenu").animate({left: "0px"});
-        sideMenu = false;
-    }
-});*/
