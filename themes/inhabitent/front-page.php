ff20cabf0e85386df4ef41d2731372a5e303ae2e
justinl-y <j@solaris.org.uk>
2016-11-12 10:10:54 -0800

    Front-page Shop Stuff styles added

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -22,14 +22,13 @@ get_header(); ?>
                     $terms = get_terms('product_type');
 
                     foreach ($terms as $term) : ?>
-
-                        <?php //echo print_r($term); ?>
-
                         <?php $url = get_term_link($term->slug, 'product_type'); ?>
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/product-type-icons/<?php echo $term->slug; ?>.svg">
 
-                        <p><?php echo $term->description; ?></p>
-                        <p><a href="<?php echo $url ?>"><?php echo $term->name; ?> Stuff</a></p>
+                        <div class="product-feed-item">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/product-type-icons/<?php echo $term->slug; ?>.svg">
+                            <p><?php echo $term->description; ?></p>
+                            <p><a href="<?php echo $url ?>"><?php echo $term->name; ?> Stuff</a></p>
+                        </div>
                     <?php endforeach;
                 ?>
             </section>
@@ -91,7 +90,6 @@ get_header(); ?>
                 );
 
                 $adventure_posts = get_posts( $args );
-                //$adventure_post_number = 1;
                 $adventure_posts_html = [];
 
                 foreach ( $adventure_posts as $post ) : setup_postdata( $post );
@@ -103,15 +101,9 @@ get_header(); ?>
                         <h3><a class="home-adventures-link-title" href="<?php the_permalink();?>"><?php the_title(); ?></a></h3>
                         <a class="home-adventures-link-button" href="<?php the_permalink();?>">Read More</a>
 
-                    <?php array_push( $adventure_posts_html, ob_get_clean() ); ?>
+                    <?php array_push( $adventure_posts_html, ob_get_clean() );
 
-                    <?php //$adventure_post_number++;
-
-                 endforeach; wp_reset_postdata(); //echo print_r($adventure_posts_html); ?>
-
-
-                <!--<div class="<?php //echo 'home-adventures-image-' . $adventure_post_number; ?>">
-                </div>-->
+                 endforeach; wp_reset_postdata(); ?>
 
 
             <div class="home-adventures-images">
@@ -138,56 +130,6 @@ get_header(); ?>
             </div>
 
             <a href="<?php echo get_post_type_archive_link('adventure'); ?>">More Adventures</a>
-
-            <!--<div class="home-adventures-images">
-
-                <div class="home-adventures-column-left">
-                    <div class="home-adventures-column-left-top">
-                        <span class="home-adventures-image-text">Getting Back to Nature in a Canoe</span>
-                    </div>
-                </div>
-
-                <div class="home-adventures-column-right">
-                    <div class="home-adventures-column-right-top">
-                        <span class="home-adventures-image-text">A Night with Friends at the Beach</span>
-                    </div>
-
-                    <div class="home-adventures-column-right-bottom-left">
-                        <span class="home-adventures-image-text">Taking in the View at Big Mountain</span>
-                    </div>
-
-                    <div class="home-adventures-column-right-bottom-right">
-                        <span class="home-adventures-image-text">Star-Gazing at the Night Sky</span>
-                    </div>
-                </div>
-            </div>
-
-            <!--<div class="latest-adventures">
-                <h2 class="front-page-h2">Latest Adventures</h2>
-
-                <div class="adventures-container">
-                    <div class="leftside-adventures">
-                        <p>Getting Back to Nature in a Canoe</p>
-                        <span class="static-read-more">Read More</span>
-                    </div>
-
-                    <div class="rightside-adventures">
-                        <div class="rightside-top-adventure">
-                            <p>A Night with Friends at the Beach</p>
-                            <span class="static-read-more">Read More</span>
-                        </div>
-                        <div class="bottom-left-rightside-adventure">
-                            <p>Taking in the View at Big Mountain</p>
-                            <span class="static-read-more">Read More</span>
-                        </div>
-                        <div class="bottom-right-rightside-adventure">
-                            <p>Star-Gazing at the Night Sky</p>
-                            <span class="static-read-more">Read More</span>
-                        </div>
-                    </div>
-                </div>
-                <p class="more-adventures"> More Adventures</p>
-            </div>-->
 
         </section>
     </div>
--- themes/inhabitent/taxonomy-product_type.php
+++ themes/inhabitent/taxonomy-product_type.php
@@ -12,14 +12,14 @@ get_header(); ?>
 
 			<div class="container">
 
-				<p>taxonomy-product-type.php</p>
+				<p>taxonomy-product_type.php</p>
 
 				<?php if ( have_posts() ) : ?>
 
 					<header class="page-header">
 						<?php
-						the_archive_title( '<h1 class="page-title">', '</h1>' );
-						the_archive_description( '<div class="taxonomy-description">', '</div>' );
+							the_archive_title( '<h1 class="page-title">', '</h1>' );
+							the_archive_description( '<div class="taxonomy-description">', '</div>' );
 						?>
 					</header><!-- .page-header -->
 
