d95f8ebc00e59517de086adc347067291aeeda93
justinl-y <j@solaris.org.uk>
2016-11-11 20:59:02 -0800

    Header search form css added

--- themes/inhabitent/searchform.php
+++ themes/inhabitent/searchform.php
@@ -1,13 +1,26 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+
 	<fieldset>
+		<a href="#" class="search-toggle" aria-hidden="true">
+			<i class="fa fa-search"></i>
+		</a>
 		<label>
-			<input type="search" class="search-field" placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
+			<input type="search" class="search-field" placeholder="TYPE AND HIT ENTER..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
 		</label>
-		<button class="search-submit">
+		<input type="submit" id="search-submit" class="screen-reader-text" value="Search">
+	</fieldset>
+
+</form>
+
+
+<!--<fieldset>
+	<label>
+		<input type="search" class="search-field" placeholder="TYPE AND HIT ENTER..." value="<?php //echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
+	</label>
+	<button class="search-submit">
 			<span class="icon-search" aria-hidden="true">
 				<i class="fa fa-search"></i>
 			</span>
-			<span class="screen-reader-text"><?php echo esc_html( 'Search' ); ?></span>
-		</button>
-	</fieldset>
-</form>
+		<span class="screen-reader-text"><?php //echo esc_html( 'Search' ); ?></span>
+	</button>
+</fieldset>-->
