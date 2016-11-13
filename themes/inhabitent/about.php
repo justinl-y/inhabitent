3e0fd4f9b4e52a789c3657de6fd2155168949bc2
justinl-y <j@solaris.org.uk>
2016-11-13 14:46:01 -0800

    About page styles added in additional sass partial

--- themes/inhabitent/about.php
+++ themes/inhabitent/about.php
@@ -9,12 +9,16 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 			<!--about.php -->
 			<header class="about-hero">
-				<h1>About</h1>
+				<div class=" container">
+					<h1>About</h1>
+				</div>
 			</header>
 
 			<section class="container">
-				<div class="about-our-story"><?php echo CFS()->get('our_story') ; ?></div>
-				<div class="about-our-team"><?php echo CFS()->get('our_team') ; ?></div>
+				<div class="about-content">
+					<div class="about-our-story"><?php echo CFS()->get('our_story') ; ?></div>
+					<div class="about-our-team"><?php echo CFS()->get('our_team') ; ?></div>
+				</div>
 			</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
