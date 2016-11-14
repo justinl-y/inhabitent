aa568e26532cf347a95494eb0c50cbf31ed8b120
justinl-y <j@solaris.org.uk>
2016-11-13 21:44:29 -0800

    404 page styles added in sass partial.

--- themes/inhabitent/404.php
+++ themes/inhabitent/404.php
@@ -9,50 +9,49 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+<div class="container page-not-found">
+	<div id="primary" <?php body_class('page-not-found-content-area'); ?>>
+
 		<main id="main" class="site-main" role="main">
-			<div class="container">
+			<section class="error-404 not-found">
+				<header class="page-header">
+					<h1 class="page-title"><?php echo esc_html( 'Oops! That page can&rsquo;t be found.' ); ?></h1>
+				</header><!-- .page-header -->
 
-				<section class="error-404 not-found">
-					<header class="page-header">
-						<h1 class="page-title"><?php echo esc_html( 'Oops! That page can&rsquo;t be found.' ); ?></h1>
-					</header><!-- .page-header -->
+				<div class="page-content">
+					<p><?php echo esc_html( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?' ); ?></p>
 
-					<div class="page-content">
-						<p><?php echo esc_html( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?' ); ?></p>
+					<?php get_search_form(); ?>
 
-						<?php get_search_form(); ?>
+					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
-						<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+					<?php if ( red_starter_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
+					<div class="widget widget_categories">
+						<h2 class="widget-title"><?php echo esc_html( 'Most Used Categories' ); ?></h2>
+						<ul>
+							<?php
+								wp_list_categories( array(
+									'orderby'    => 'count',
+									'order'      => 'DESC',
+									'show_count' => 1,
+									'title_li'   => '',
+									'number'     => 10,
+								) );
+							?>
+						</ul>
+					</div><!-- .widget -->
+					<?php endif; ?>
 
-						<?php if ( red_starter_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-						<div class="widget widget_categories">
-							<h2 class="widget-title"><?php echo esc_html( 'Most Used Categories' ); ?></h2>
-							<ul>
-								<?php
-									wp_list_categories( array(
-										'orderby'    => 'count',
-										'order'      => 'DESC',
-										'show_count' => 1,
-										'title_li'   => '',
-										'number'     => 10,
-									) );
-								?>
-							</ul>
-						</div><!-- .widget -->
-						<?php endif; ?>
-
-						<?php
-							$archive_content = '<p>' . sprintf( esc_html( 'Try looking in the monthly archives. %1$s' ), convert_smilies( ':)' ) ) . '</p>';
-							the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-						?>
-					</div><!-- .page-content -->
-				</section><!-- .error-404 -->
-			</div><!-- .container -->
-
+					<?php
+						$archive_content = '<p>' . sprintf( esc_html( 'Try looking in the monthly archives. %1$s' ), convert_smilies( ':)' ) ) . '</p>';
+						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+					?>
+				</div><!-- .page-content -->
+			</section><!-- .error-404 -->
 		</main><!-- #main -->
 	</div><!-- #primary -->
+	<?php get_sidebar(); ?>
+</div><!-- .container -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
 
