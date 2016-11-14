554709d4ee918104b91d09a6b4b56bd9aba9bd52
justinl-y <j@solaris.org.uk>
2016-11-13 18:46:59 -0800

    Single adventure page styled. Adventures styles refactored into separate sass partial.

--- themes/inhabitent/single-adventure.php
+++ themes/inhabitent/single-adventure.php
@@ -10,22 +10,46 @@ get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 
+		<section class="advenure-hero">
+			<?php if ( has_post_thumbnail() ) : ?>
+				<?php the_post_thumbnail( 'full' ); ?>
+			<?php endif; ?>
+		</section>
+
 		<div class="container">
-			<p>single-adventure.php</p>
+			<!--<p>single-adventure.php</p>-->
+			<div class="adventure-container">
+
 			<?php while ( have_posts() ) : the_post(); ?>
+				<?php //get_template_part( 'template-parts/content', 'single' ); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="entry-header">
+						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+					</header><!-- .entry-header -->
 
-				<?php get_template_part( 'template-parts/content', 'single' ); ?>
+					<div class="entry-content">
+						<?php the_content(); ?>
+						<?php
+						wp_link_pages( array(
+							'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+							'after'  => '</div>',
+						) );
+						?>
+					</div><!-- .entry-content -->
 
-				<?php the_post_navigation(); ?>
+					<footer class="entry-footer">
+						<?php red_starter_entry_footer(); ?>
 
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-
+						<div class="social-buttons">
+							<button type="button" class="black-btn"><i class="fa fa-facebook"></i><span>Like</span></button>
+							<button type="button" class="black-btn"><i class="fa fa-twitter"></i><span>Tweet</span></button>
+							<button type="button" class="black-btn"><i class="fa fa-pinterest"></i><span>Pin</span></button>
+						</div><!-- .social-buttons -->
+					</footer><!-- .entry-footer -->
+				</article><!-- #post-## -->
 			<?php endwhile; // End of the loop. ?>
+
+			</div><!-- .container -->
 		</div><!-- .container -->
 
 	</main><!-- #main -->
