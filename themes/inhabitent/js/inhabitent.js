d082f8d22c51d4dca8e65f90040b8c83dc2716d3
justinl-y <j@solaris.org.uk>
2016-11-21 12:50:51 -0800

    Search form refactored.

--- themes/inhabitent/js/inhabitent.js
+++ themes/inhabitent/js/inhabitent.js
@@ -1,9 +1,8 @@
 (function( $ ) {
-
     $('#search-toggle').on('click', function() {
         $('#search-field').animate( { width: 'toggle' }, 500 ).focus();
     });
 
     $('#search-field').animate( { width: 'toggle' }, 0 );
-
 })( jQuery );
+
