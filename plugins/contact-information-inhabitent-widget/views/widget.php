38886260676db7c88bcc773c5896b9a244f8440e
justinl-y <j@solaris.org.uk>
2016-11-22 13:21:12 -0800

    Styles refactored.

--- plugins/contact-information-inhabitent-widget/views/widget.php
+++ plugins/contact-information-inhabitent-widget/views/widget.php
@@ -1,18 +1,50 @@
 <!-- This file is used to markup the public-facing widget. -->
-<?php if ( strlen( trim( $telephone_number ) ) > 0 ) : ?>
-	<p>
-		<i class="fa fa-phone" aria-hidden="true"></i> <a href="tel:<?php echo $telephone_number; ?>"><?php echo $telephone_number; ?></a>
-	</p>
-<?php endif; ?>
+<div class="contact-widget">
+	<?php if ( strlen( trim( $telephone_number ) ) > 0 ) : ?>
+		<div class="contact-row">
+			<div class="contact-icon">
+				<i class="fa fa-phone" aria-hidden="true"></i>
+			</div>
+			<div class="contact-detail">
+				<a href="tel:<?php echo $telephone_number; ?>"><?php echo $telephone_number; ?></a>
+			</div>
+		</div>
 
-<?php if ( strlen( trim( $email_address ) ) > 0 ) : ?>
-	<p>
-		<i class="fa fa-envelope" aria-hidden="true"></i> <a href="mailto:<?php echo $email_address; ?>"><?php echo $email_address; ?></a>
-	</p>
-<?php endif; ?>
 
-<?php if ( strlen( trim( $address ) ) > 0 ) : ?>
-	<p>
-		<i class="fa fa-map-marker" aria-hidden="true"></i> <span class="address"><?php echo $address; ?></span>
-	</p>
-<?php endif; ?>
+		<!--<p>
+			<i class="fa fa-phone" aria-hidden="true"></i> <a href="tel:<?php //echo $telephone_number; ?>"><?php //echo $telephone_number; ?></a>
+		</p>-->
+	<?php endif; ?>
+
+	<?php if ( strlen( trim( $email_address ) ) > 0 ) : ?>
+		<div class="contact-row">
+			<div class="contact-icon">
+				<i class="fa fa-envelope" aria-hidden="true"></i>
+			</div>
+			<div class="contact-detail">
+				<a href="mailto:<?php echo $email_address; ?>"><?php echo $email_address; ?></a>
+			</div>
+		</div>
+
+		<!--<p>
+			<i class="fa fa-envelope" aria-hidden="true"></i> <a href="mailto:<?php //echo $email_address; ?>"><?php //echo $email_address; ?></a>
+		</p>-->
+	<?php endif; ?>
+
+	<?php if ( strlen( trim( $address ) ) > 0 ) : ?>
+		<div class="contact-row">
+			<div class="contact-icon">
+				<i class="fa fa-map-marker" aria-hidden="true"></i>
+			</div>
+			<div class="contact-detail">
+				<span class="address"><?php echo $address; ?></span>
+			</div>
+		</div>
+	<?php endif; ?>
+</div>
+
+
+
+
+
+
